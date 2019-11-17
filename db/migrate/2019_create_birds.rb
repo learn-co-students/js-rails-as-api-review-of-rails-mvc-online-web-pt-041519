e348c64909aebf8ff1f7fcfd455a586033be0f63
github username <github email address>
2019-11-16 22:54:00 -0700

    Done.

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,9 +1,10 @@
-class CreateBirds < ActiveRecords::Migration[5.2]
+class CreateBirds < ActiveRecord::Migration[5.2]
     def change
-        create_table :birds do |t|
-            t.string :name
-            t.string :species
-            t.timestamps
-        end
+      create_table :birds do |t|
+        t.string :name
+        t.string :species
+   
+        t.timestamps
+      end
     end
-end
+  end
